fa3ed7f293f6378bbf0566df31811caec0cee5a4
Michal Ivičič <ivicic@netglade.cz>
2020-05-06 13:29:09 +0200

    + testing

---
+++ +ifov/test2.m
@@ -0,0 +1,16 @@
+ADown = [1 0 3; 0 0 0; 0 0 0];
+count = 20;
+hold on;
+B = zeros(3);
+B(1,2) = 5;
+C = zeros(3);
+C(2,3) = -5;
+for i = 0:count
+    for j= 0:count
+        A = ADown + (i/count)*(B) + (j/count)*C;
+        coords = ifov.matrixfov(A,20);
+        coords = [coords; coords(1)];
+        plot(real(coords),imag(coords));
+    end
+end
+    
