366e6fdb8e546c18efe304c74c4f4ed61149dafb
Michal Ivičič <ivicic@netglade.cz>
2020-05-20 08:26:39 +0200

    * notconvex

--- +ifov/notconvex.m
+++ +ifov/notconvex.m
@@ -5,8 +5,9 @@
 
 ADown = [0 0; 0 0];
 % AUp = [0 1; 1 0];
-count = 20;
+count = 30;
 hold on;
+box on;
 B = zeros(2);
 B(1,2) = 1;
 C = zeros(2);
@@ -14,12 +15,12 @@ C(2,1) = 1;
 for i = 0:count
     for j= 0:count
         A = ADown + (i/count)*(B) + (j/count)*C;
-        coords = ifov.matrixfov(A,20);
+        coords = ifov.matrixfov(A,50);
         coords = [coords; coords(1)];
-        plot(real(coords),imag(coords));
+        plot(real(coords),imag(coords),'Color',[0.5,0.5,0.5]);
     end
 end
 
-coords = ifov.imatrixfov(ADown,ADown + B + C, 20);
+coords = ifov.imatrixfov(ADown,ADown + B + C, 50);
 coords = [coords; coords(1)];
-plot(real(coords),imag(coords), 'LineWidth', 3);
+plot(real(coords),imag(coords), 'LineWidth', 2, 'Color', 'k');
